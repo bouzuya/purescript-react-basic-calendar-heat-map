475cc67878f337e1ea94dd50db624fa3536bdfa2
bouzuya <m@bouzuya.net>
2019-02-03 13:59:35 +0900

    add some styles

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -4,8 +4,10 @@ module Component.App
 
 import Prelude
 
+import Component.AppStyle as Style
 import Data.Array as Array
 import React.Basic (Component, JSX, Self, StateUpdate(..), createComponent, make)
+import React.Basic.DOM (css)
 import React.Basic.DOM as H
 
 type Props =
@@ -46,25 +48,46 @@ render self =
           [ H.span_ [ H.text "data" ]
           , H.textarea_ []
           ]
-        , H.table_
-          [ H.thead_
-            []
-          , H.tbody_
-            ( flip Array.mapWithIndex
-              ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-              (\weekdayNumber weekdayString ->
-                H.tr_
-                ( [ H.th_ [ H.text weekdayString ] ] <>
-                  ( (Array.range 0 53) <#>
-                    (\_ -> -- weekNumber
-                      H.td_
-                      [ H.span_ [ H.text "0" ] ] -- data[weekNumber][weekdayNumber]
+        , H.table
+          { className: Style.table
+          , children:
+            [ H.thead_
+              []
+            , H.tbody_
+              ( flip Array.mapWithIndex
+                ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+                (\weekdayNumber weekdayString ->
+                  H.tr_
+                  ( [ H.th
+                      { className: Style.th
+                      , children: [ H.text weekdayString ]
+                      }
+                    ] <>
+                    ( (Array.range 0 53) <#>
+                      (\_ -> -- weekNumber
+                        let
+                          value = 0
+                          color v = case v of
+                            0 -> "#f00"
+                            _ -> "transparent"
+                        in
+                          H.td
+                          { className: Style.td
+                          , children:
+                            [ H.span
+                              { className: Style.value
+                              , children: [ H.text (show value) ]
+                              , style: css { backgroundColor: color value }
+                              }
+                            ] -- data[weekNumber][weekdayNumber]
+                          }
+                      )
                     )
                   )
                 )
               )
-            )
-          ]
+            ]
+          }
         ]
       }
     , H.div
